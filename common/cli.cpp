bc02950f36e0e62fd21fdbfedc585765efffd3f1
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2016-12-24 22:49:49 +0200

    CLI

---
+++ common/cli.cpp
@@ -0,0 +1,62 @@
+#include "cli.h"
+#include <cli/Actions.h>
+
+
+namespace cli
+{
+	// global variables
+	namespace internal
+	{
+		std::unique_ptr<CLIServer> processorCLIServer;
+	}
+
+
+    void addAction(const char*, const char*, ActionHandler)
+    {
+    	// TODO:...
+    }
+
+
+    Server& getCLIServer()
+	{
+		return *internal::processorCLIServer->getResource();
+	}
+
+
+	void initializeCLIServer(unsigned int port, unsigned int maxConnections, g3::LogWorker *logWorkerPtr)
+	{
+		// TODO: lock
+		// TODO: only once
+		internal::processorCLIServer = std::make_unique<conwrap::ProcessorAsio<Server>>(15673, 2, logWorkerPtr);
+
+		// registering default actions
+		auto actions = getCLIServer().getActions();
+		actions.addDefaultCLIActions();
+		actions.addDefaultLogActions();
+	}
+
+
+	void startCLIServer()
+	{
+		// starting CLI server on its separate thread
+		internal::processorCLIServer->process(
+			[](auto context) {
+				context.getResource()->start();
+			}
+		);
+	}
+
+
+	void stopCLIServer()
+	{
+		// stopping CLI server
+		internal::processorCLIServer->process(
+			[](auto context) {
+				context.getResource()->stop();
+			}
+		);
+
+		// deleting CLI server; it will wait for all tasks to complete
+		internal::processorCLIServer.reset();
+	}
+}
---
+++ common/cli.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cli/Action.h>
+#include <cli/Server.h>
+#include <conwrap/ProcessorAsio.hpp>
+#include <g3log/logworker.hpp>
+
+
+namespace cli
+{
+	using CLIServer = conwrap::ProcessorAsio<Server>;
+
+
+    void    addAction(const char*, const char*, ActionHandler);
+	Server& getCLIServer();
+	void    initializeCLIServer(unsigned int port, unsigned int maxConnections, g3::LogWorker* logWorkerPtr);
+	void    startCLIServer();
+	void    stopCLIServer();
+}
